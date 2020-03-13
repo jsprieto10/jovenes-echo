ae9f454faf7295e080b3a4fb7636787144077a75
jsprieto10 <js.prieto10@uniandes.edu.co>
2020-03-13 00:07:12 -0500

    primer commit

---
+++ Segunda/scroll_manager.js
@@ -0,0 +1,61 @@
+$('html, body').animate({
+    scrollTop: $("#sect" + 0).offset().top - 85
+}, 1000);
+var act_sect = 0;
+var max_sect = 2;
+var wait_im_scrolling = false;
+$('#sect' + (act_sect + 1)).css('visibility', 'hidden');
+$('#sect' + (act_sect + 2)).css('visibility', 'hidden');
+
+function cambiar_pos_directamente(targetId) {
+    intTarget = parseInt(targetId.split("sect")[1]);
+    while (intTarget != act_sect) {
+        if (act_sect >= intTarget) {
+            subir_scroll();
+        } else {
+            bajar_scroll();
+        }
+    }
+}
+
+function subir_scroll() {
+    //wait_im_scrolling = true;
+
+    act_sect = Math.max(0, act_sect-1);
+
+    d3.select("#titulo_footer").text("Segundo Encuentro: Retos");
+
+
+    $('#sect' + (act_sect)).css('visibility', 'visible');
+    $('html, body').animate({
+        scrollTop: $("#sect" + act_sect).offset().top - 85
+    }, 500);
+    $('#sect' + (act_sect + 1)).css('visibility', 'hidden');
+}
+
+function bajar_scroll(){
+
+
+    act_sect = 1
+    d3.select("#titulo_footer").text("Segundo Encuentro: Propuestas");
+
+    $('#sect' + (act_sect)).css('visibility', 'visible');
+    $('html, body').animate({
+        scrollTop: $("#sect" + act_sect).offset().top - 85
+    }, 500);
+    $('#sect' + (act_sect - 1)).css('visibility', 'hidden');
+
+}
+
+
+
+document.onkeydown = function(e) {
+    switch (e.keyCode) {
+        case 38:
+            subir_scroll();
+            break;
+        case 40:
+            bajar_scroll();
+            break;
+    }
+};
